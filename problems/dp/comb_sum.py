b87d63678515304c6c649b1689234406c93e44cb
smolbrain <randomlyrandamu@gmail.com>
2022-12-31 21:39:03 +1100

    AAAAAAAAAAAA

--- problems/dp/comb_sum.py
+++ problems/dp/comb_sum.py
@@ -24,3 +24,14 @@ class Solution:
             ans.append(level)
         
         return ans
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [[] for _ in range(target+1)]
+        for c in candidates:
+            for i in range(c, target+1):
+                if i == c: 
+                    dp[i].append([c])
+                for comb in dp[i-c]: 
+                    dp[i].append(comb + [c])
+        return dp[-1]

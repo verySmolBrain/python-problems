1c86f5addde40b179b2ac0efe7fbf0c727838a38
smolbrain <randomlyrandamu@gmail.com>
2023-01-05 18:02:02 +1100

    AAAA

--- problems/search/word_break.py
+++ problems/search/word_break.py
@@ -10,3 +10,14 @@ class Solution:
                 return True
         
         return False
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        canMake = [False] * len(s)
+
+        for i in range(len(s)):
+            for word in wordDict:
+                if (word == s[i - len(word) + 1:i + 1] and canMake[i - len(word)]) or (i - len(word) == -1):
+                    canMake[i] = True
+
+        return canMake[-1]
